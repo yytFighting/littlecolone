7ab0c2ffac4dc863cfcc17e471ca2a83d3be8edd
qrzj <1774878535@qq.com>
2020-08-30 13:01:39 +0800

    Updates

---
+++ precache-manifest.ae92a745ab2f828cd2254414de6fad29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2e0ea9e20ac814b1b033cc60b9e2bf",
+    "url": "/littlecolone/index.html"
+  },
+  {
+    "revision": "b392376f52a64660e4a2",
+    "url": "/littlecolone/static/css/2.fb92a525.chunk.css"
+  },
+  {
+    "revision": "1c43ab8ae4c02aac0a4f",
+    "url": "/littlecolone/static/css/main.b0e79027.chunk.css"
+  },
+  {
+    "revision": "b392376f52a64660e4a2",
+    "url": "/littlecolone/static/js/2.c0fa6539.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/littlecolone/static/js/2.c0fa6539.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c43ab8ae4c02aac0a4f",
+    "url": "/littlecolone/static/js/main.dcf2125d.chunk.js"
+  },
+  {
+    "revision": "b43579da72cc04e5579c",
+    "url": "/littlecolone/static/js/runtime-main.9ae2e68e.js"
+  }
+]);
